b96bc9272cb3422d3e2cb3b54a54a8fa4b5891af
lielmenag <lielmenag@gmail.com>
2025-09-08 14:18:20 +0300

    Commit new name

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('liel')
+    print_hi('iraz')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
